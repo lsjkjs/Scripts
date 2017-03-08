5821e76d53504f7d4c6a48fcb07fad7bfbacd9a7
Jin Sun Kim <kimjins7@gmail.com>
2017-03-08 14:42:02 -0800

    added script for lab 9

---
+++ scripts/lab-9-script.R
@@ -0,0 +1,2 @@
+download.file("https://raw.githubusercontent.com/swcarpentry/r-novice-gapminder/gh-pages/_episodes_rmd/data/gapminder-FiveYearData.csv", destfile = "gapminder-FiveYearData.csv")
+gapminder <- read.csv(file = "gapminder-FiveYearData.csv")
